30d9253d19f8ee12868fb5b6430dcb2ec6bce4dd
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2021-02-04 15:32:41 -0300

    feat: add question method, to get user input

---
+++ src/question.ts
@@ -0,0 +1,19 @@
+import { createInterface } from 'readline';
+
+const readline = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.on('close', function () {
+  console.log('\nClosing...');
+});
+
+export function question(message: string) {
+  return new Promise(resolve => {
+    readline.question(message, answer => {
+      resolve(answer);
+      readline.close();
+    });
+  });
+}
