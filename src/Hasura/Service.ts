2a4250660ca2d494f453d0cb749c09183eadd203
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2021-02-04 16:51:10 -0300

    feat: add service method to get remote allowed query collection

--- src/Hasura/Service.ts
+++ src/Hasura/Service.ts
@@ -1,4 +1,6 @@
 import { QueryCollection } from './QueryCollection';
+import { getAllowedQueryCollection } from './Metadata';
+import { Api } from './Api';
 
 export function hasuraService(api: Api) {
   return {
@@ -7,6 +9,11 @@ export function hasuraService(api: Api) {
     ): Promise<any> {
       await api.dropQueryFromCollection(collectionQuery);
       return api.addQueryToCollection(collectionQuery);
+    },
+    async getRemoteAllowedQueryCollection() {
+      const metadata = (await api.exportMetadata()).data;
+      const remoteQueries = await getAllowedQueryCollection(metadata);
+      return remoteQueries;
     },
   };
 }
