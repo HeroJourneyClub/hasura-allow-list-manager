f6338e86ee9d3f7ae2363c79df5d2cfd67fa7e2f
Marc Simon <marc.simon42@gmail.com>
2021-11-09 13:46:45 +0000

    Allow multiple path

--- src/cli.ts
+++ src/cli.ts
@@ -5,7 +5,7 @@ const program = new Command();
 program
   .option('-h, --host <host>', 'Hasura URI host')
   .option('-s, --admin-secret <key>', 'Hasura adming secret key')
-  .option('-p, --path <path>', 'Source path with gql or graphql files')
+  .option('-p, --path <path...>', 'Source path with gql or graphql files')
   .option(
     '-f, --force-replace',
     'Replace change queries, not asking for continue'
